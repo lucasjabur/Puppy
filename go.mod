2bd96b018b7077cacfbe6bcab6c637ddb562ad7f
Lucas Cardoso Jabur <lucas.cardoso.jabur@outlook.com>
2025-08-29 08:58:32 -0300

    UPDATE: Uploading new auxiliar functions with Dog as a dependecy for usage in Learning-Golang repo

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/lucasjabur/Puppy
 
 go 1.25.0
+
+require github.com/lucasjabur/Dog v0.0.0-20250829115157-49a61b0ba46c // indirect
--- puppy.go
+++ puppy.go
@@ -1,9 +1,21 @@
 package puppy
 
+import (
+	dog "github.com/lucasjabur/Dog"
+)
+
 func Bark() string {
 	return "Woof!"
 }
 
 func Barks() string {
 	return "Woof! Woof! Woof!"
+}
+
+func BigBark() string {
+	return dog.WhenGrownUp(Bark())
+}
+
+func BigBarks() string {
+	return dog.WhenGrownUp(Barks())
 }
